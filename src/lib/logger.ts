27475701097b82e2b7eaade611836ea75ef58225
bhaveshsinghal95182 <work.bhavesh22@gmail.com>
2025-09-01 17:38:20 +0000

    refactor: removed pino pretty cuz it was causing error with thread-stream

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -7,17 +7,17 @@ const isProduction = env.NODE_ENV === "production";
 
 export const logger = pino({
   level: env.LOG_LEVEL || "info",
-  transport:
-    !isEdge && !isProduction
-      ? {
-          target: "pino-pretty",
-          options: {
-            colorize: true,
-            ignore: "pid,hostname",
-            translateTime: "SYS:standard",
-          },
-        }
-      : undefined,
+  // transport:
+  //   !isEdge && !isProduction
+  //     ? {
+  //         target: "pino-pretty",
+  //         options: {
+  //           colorize: true,
+  //           ignore: "pid,hostname",
+  //           translateTime: "SYS:standard",
+  //         },
+  //       }
+  //     : undefined,
   formatters: {
     level: label => ({level: label.toUpperCase()}),
   },
